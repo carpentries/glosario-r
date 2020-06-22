46dc2779812d43e77fe6ed82a592119fa664423d
François Michonneau <francois.michonneau@gmail.com>
2020-06-22 09:16:46 +0200

    fix package doc

--- R/glossary.R
+++ R/glossary.R
@@ -1,6 +1,10 @@
 #' A multilingual glossary.
 #'
-#' @author Greg Wilson, \email{greg.wilson@rstudio.com}
+#' `glosario` provides the infrastructure to create and read multilingual
+#' glossaries. By default, `glosario` providesaccess to a community-curated
+#' glossary hosted by [The
+#' Carpentries](https://github.com/carpentries/glosario).
+#'
 #' @docType package
 #' @name glossary
 #' @importFrom tibble `%>%`
