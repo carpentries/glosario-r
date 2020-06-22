9b5d309cdcb89df5afe0917256accc6a0480b20c
François Michonneau <francois.michonneau@gmail.com>
2020-06-22 14:46:44 +0200

    restructure caching function

--- R/cache.R
+++ R/cache.R
@@ -17,16 +17,26 @@ write_cache <- function(glossary_path, cached_glossary_path) {
   to_cache
 }
 
+generate_cached_glossary_path <- function(glossary_path, cache_path) {
+  ## we're only hashing on the path.
+  ## Hashing on the content would be more robust but it defeats the
+  ## purpose of caching in the first place as we'd have to parse the
+  ## entire content of the file.
+  hashed_glossary_path <- digest::digest(tolower(glossary_path))
+  cached_glossary_file_name <- paste0(
+    "glossary-", hashed_glossary_path, ".rds"
+  )
+  file.path(cache_path, cached_glossary_file_name)
+}
 
 use_cache <- function(glossary_path, cache_path) {
-  hash_glossary_path <- digest::digest(tolower(glossary_path))
 
   validate_glossary_cache_path(cache_path)
 
-  cached_glossary_file_name <- paste0(
-    "glossary-", hash_glossary_path, ".rds"
+  cached_glossary_path <- generate_cached_glossary_path(
+    glossary_path,
+    cache_path
   )
-  cached_glossary_path <- file.path(cache_path, cached_glossary_file_name)
 
   if (file.exists(cached_glossary_path)) {
     raw_glossary <- read_cache(cached_glossary_path)
